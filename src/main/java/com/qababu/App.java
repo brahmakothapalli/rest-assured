8b8ef6379e48e5e37903afbb4218508770d7f0f1
Venkata Brahma Rao Kottapalli <KVenkata@royalsundaram.in>
2019-09-13 13:57:53 +0530

    project

---
+++ src/main/java/com/qababu/App.java
@@ -0,0 +1,13 @@
+package com.qababu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
